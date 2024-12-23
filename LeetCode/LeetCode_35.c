f20d4346d89835e0f1aeaeedf273ed7c87aeaa98
Kim-Hyo-Jeong <lk7938@naver.com>
2024-12-23 17:10:48 +0900

    LeetCode 35

--- LeetCode/LeetCode_35.c
+++ LeetCode/LeetCode_35.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 int searchInsert(int *nums, int numsSize, int target)
 {
     int start = 0, end = numsSize - 1;
@@ -17,26 +15,4 @@ int searchInsert(int *nums, int numsSize, int target)
     }
 
     return start;
-}
-
-int main(void)
-{
-    int numsSize, target;
-
-    printf("배열의 크기 입력: ");
-    scanf("%d", &numsSize);
-
-    int nums[numsSize];
-
-    printf("배열의 요소 입력: ");
-    for (int i = 0; i < numsSize; i++)
-        scanf("%d", &nums[i]);
-
-    printf("target 값 입력: ");
-    scanf("%d", &target);
-
-    int result = searchInsert(nums, numsSize, target);
-    printf("%d\n", result);
-
-    return 0;
 }
